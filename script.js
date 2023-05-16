5f648c568e88225be2c5d0c18380cddb5d291d3f
Corbijn Bulsink <corbijn.bulsink@hotmail.com>
2023-05-15 18:01:40 -0700

    added createGrid function and userinput for gridsize

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
-const board = document.querySelector("#board");
+const board = document.getElementById('board');
+var gridSize = document.getElementById('gridsize');
+var userSubmit = document.getElementById('user-submit');
 
-
-
+//var sizeAlert = document.getElementById('size-alert');
 //var isDrawing = false;
 
-for (i = 0; i < 256; i++) {
-    const cell = document.createElement("div");
-    cell.id = "cellBoard";
+userSubmit.addEventListener("click", () => {
+    let size = gridSize.value;
+    createGrid(size);
+});
 
-    cell.addEventListener("mouseover", () =>
-        cell.setAttribute("style", "background-color:blue;"));
+function createGrid(size) {
+    console.log(gridSize.value);
+    // var numberOfCells = gridSize.value;
+    let totalCells = size * size;
 
-    // cell.addEventListener("mousedown", () => {
-    //     holdingMouse();
-    // });
-    
-    // function holdingMouse() {
-    //     isDrawing = !isDrawing;
-    //     console.log(isDrawing);
-    // }
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    board.appendChild(cell);
+    for (let i = 0; i < totalCells; i++) {
+
+        let cell = document.createElement("div");
+        cell.addEventListener("mouseover", () =>
+            cell.setAttribute("style", "background-color:blue;"));
+        cell.id = "cellBoard";
+        board.appendChild(cell);
+    }
+
+    console.log(totalCells);
 }
+
+
+// for (i = 0; i < 256; i++) {
+//     const cell = document.createElement("div");
+//     cell.id = "cellBoard";
+
+    // cell.addEventListener("mouseover", () =>
+    //     cell.setAttribute("style", "background-color:blue;"));
+
+//     // cell.addEventListener("mousedown", () => {
+//     //     holdingMouse();
+//     // });
+    
+//     // function holdingMouse() {
+//     //     isDrawing = !isDrawing;
+//     //     console.log(isDrawing);
+//     // }
+
+//     board.appendChild(cell);
+// }
