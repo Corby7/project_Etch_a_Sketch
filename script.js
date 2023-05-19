e763323890ed769cbc9de11b686f5171301b2289
Corbijn Bulsink <corbijn.bulsink@hotmail.com>
2023-05-18 23:07:52 -0700

    cleaned up code css & javascript, finalized ui and added function for Enter key

--- script.js
+++ script.js
@@ -1,18 +1,75 @@
-const board = document.getElementById('board');
-var gridSize = document.getElementById('gridsize');
-var userSubmit = document.getElementById('user-submit');
+let board = document.getElementById("board");
+let gridSize = document.getElementById("gridsize");
+let colorpicker = document.getElementById("colorpicker"); 
+let copyInput = document.getElementById("copy-input");
+let sizeAlert = document.getElementById("size-alert");
 
-//var sizeAlert = document.getElementById('size-alert');
-//var isDrawing = false;
+//current selected color
+let pickedColor = "black";
 
-userSubmit.addEventListener("click", () => {
-    let size = gridSize.value;
-    createGrid(size);
-});
+mirrorInput();
+createBoard(16);
 
-function createGrid(size) {
-    console.log(gridSize.value);
-    // var numberOfCells = gridSize.value;
+//buttons
+
+//shows(onfocus) or hides(onblur) warning about grid size on input
+focusWarning = () => sizeAlert.textContent = "Enter a number between 2 and 100.";
+
+blurWarning = () => sizeAlert.textContent = "";
+
+//mirrors user input to represent row x column
+function mirrorInput() {
+    gridSize.value
+    copyInput.textContent = "x " + gridSize.value;
+}
+
+//lets user submit grid size by pressing the "Enter" key
+gridSize.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("user-submit").click();
+    }
+})
+
+//drawing options
+
+//draw with black/default color
+drawBlack = () => pickedColor = "black";
+
+//pick rgb color to draw with
+drawPickedColor = () => pickedColor = colorpicker.value;
+
+//picks random color to draw with
+function drawRandom() {
+    let allCells = document.querySelectorAll("#cellBoard");
+    allCells.forEach((div) => {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    pickedColor = "#" + randomColor;
+    });
+    document.getElementById("random");
+    random.style.backgroundColor = pickedColor;
+}
+
+//input grid size & create board
+function submitSize() {
+    if (gridSize.value < 2 || gridSize.value > 100) {
+        sizeAlert.textContent = "Number is not between 2 and 100!";
+   } else if (gridSize.value > 2 || gridSize.value < 100) {
+        clearBoard();
+        createBoard(gridSize.value);
+   } else {
+        sizeAlert.textContent = "Please enter a correct numerical value!";
+   }
+}
+
+//clears board by replacing all div's with original color
+function clearBoard() {
+    let allCells = document.querySelectorAll("#cellBoard");
+    allCells.forEach((div) => {div.style.backgroundColor = "white"});
+}
+
+//creates board's grid size with user's input
+function createBoard(size) {
     let totalCells = size * size;
 
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -21,31 +78,14 @@ function createGrid(size) {
     for (let i = 0; i < totalCells; i++) {
 
         let cell = document.createElement("div");
-        cell.addEventListener("mouseover", () =>
-            cell.setAttribute("style", "background-color:blue;"));
+        
+        function draw() {
+            cell.addEventListener("mouseover", () =>
+                cell.style.backgroundColor = pickedColor);
+        }
+        draw();
+
         cell.id = "cellBoard";
         board.appendChild(cell);
     }
-
-    console.log(totalCells);
 }
-
-
-// for (i = 0; i < 256; i++) {
-//     const cell = document.createElement("div");
-//     cell.id = "cellBoard";
-
-    // cell.addEventListener("mouseover", () =>
-    //     cell.setAttribute("style", "background-color:blue;"));
-
-//     // cell.addEventListener("mousedown", () => {
-//     //     holdingMouse();
-//     // });
-    
-//     // function holdingMouse() {
-//     //     isDrawing = !isDrawing;
-//     //     console.log(isDrawing);
-//     // }
-
-//     board.appendChild(cell);
-// }
